a62723ee91253aad3e251c0fc013102d8c272bb5
Eg-Nickname <djkubax@op.pl>
2026-01-10 14:36:58 +0100

    comments and code clean up

--- N06/src/main.cpp
+++ N06/src/main.cpp
@@ -18,15 +18,15 @@ Eigen::VectorXcd deflate(complex z, const Eigen::VectorXcd &coef) {
     int n = coef.size() - 1;
     Eigen::VectorXcd b(n);
 
+    // Solving with forward substitution
     b(0) = coef(0);
-
     for (int i = 1; i < n; i++) {
         b(i) = coef(i) + z * b(i - 1);
     }
-
     return b;
 }
 
+// Evaluate polynomial using horner algorithm
 complex horner_evaluate(complex z, const Eigen::VectorXcd &coef) {
     complex P = coef(0);
     for (int i = 1; i < coef.size(); i++) {
@@ -35,6 +35,7 @@ complex horner_evaluate(complex z, const Eigen::VectorXcd &coef) {
     return P;
 }
 
+// Class to hold derivatives of current evaluated polynomial
 class Laguerre {
     Eigen::VectorXcd coef;
     Eigen::VectorXcd first_deriv_coef;
@@ -46,8 +47,6 @@ class Laguerre {
         this->first_deriv_coef = calc_derivative_coef(coef);
         this->second_deriv_coef = calc_derivative_coef(this->first_deriv_coef);
     }
-
-    double get_n() { return (double)this->coef.size() - 1; }
 
     complex solve() { return solve(complex(0.1, 0.1)); }
     complex solve(complex guess) {
@@ -62,6 +61,7 @@ class Laguerre {
 
             complex sqrt = std::sqrt(
                 (n - 1.0) * ((n - 1.0) * pd_z * pd_z - n * p_z * pdd_z));
+
             complex m1 = pd_z + sqrt;
             complex m2 = pd_z - sqrt;
 
@@ -75,63 +75,60 @@ class Laguerre {
                 z_n1 = z_n - ((n * p_z) / m1);
             }
         } while (std::abs(z_n - z_n1) > 1e-8);
+
         return z_n1;
     }
 };
 
-// complex laguerre(const Eigen::VectorXcd &coef, complex starting) {}
 std::vector<complex> lag(const Eigen::VectorXcd &coef) {
-    std::vector<complex> sol{};
-    Laguerre P = Laguerre(coef);
-    sol.push_back(P.solve());
-    // We dont need to smooth first
-    Eigen::VectorXcd cur_coef = deflate(sol.back(), coef);
+    std::vector<complex> roots{};
 
+    Laguerre P = Laguerre(coef);
+    // We dont need to smooth first root
+    roots.push_back(P.solve());
+
+    Eigen::VectorXcd cur_coef = deflate(roots.back(), coef);
     while (cur_coef.size() > 3) {
         // Calculate root
         Laguerre P_n1 = Laguerre(cur_coef);
         auto z = P_n1.solve();
         // smooth it
         auto z_smooth = P.solve(z);
-        sol.push_back(z_smooth);
+        roots.push_back(z_smooth);
         // deflate
         cur_coef = deflate(z_smooth, cur_coef);
     }
+
     // Calculate 2 remaning polynomial roots
-    complex discriminant =
+    complex delta =
         std::sqrt(cur_coef(1) * cur_coef(1) - 4.0 * cur_coef(0) * cur_coef(2));
 
-    complex z1 = (-cur_coef(1) + discriminant) / (2.0 * cur_coef(0));
-    complex z2 = (-cur_coef(1) - discriminant) / (2.0 * cur_coef(0));
+    complex z1 = (-cur_coef(1) + delta) / (2.0 * cur_coef(0));
+    complex z2 = (-cur_coef(1) - delta) / (2.0 * cur_coef(0));
 
     // Smoothing of calculated roots
-    sol.push_back(P.solve(z1));
-    sol.push_back(P.solve(z2));
+    roots.push_back(P.solve(z1));
+    roots.push_back(P.solve(z2));
 
-    return sol;
+    return roots;
 }
-// (complex)(0)); }
 
 auto main() -> int {
-    Eigen::VectorXcd v = Eigen::VectorXcd::Zero(8);
-    v(0) = 243;
-    v(1) = -486;
-    v(2) = 783;
-    v(3) = -990;
-    v(4) = 558;
-    v(5) = -28;
-    v(6) = -72;
-    v(7) = 16;
-    auto l = Laguerre(v);
-    // std::cout << l.horner_evaluate((complex)(-2.4), v);
-    // std::cout << l.solve();
-    // auto deflated = deflate(l.solve(), v);
-    auto sol = lag(v);
+    Eigen::VectorXcd init_coef = Eigen::VectorXcd::Zero(8);
+    init_coef(0) = 243;
+    init_coef(1) = -486;
+    init_coef(2) = 783;
+    init_coef(3) = -990;
+    init_coef(4) = 558;
+    init_coef(5) = -28;
+    init_coef(6) = -72;
+    init_coef(7) = 16;
+
+    auto sol = lag(init_coef);
     for (auto c : sol) {
         std::cout << c << ", ";
     }
     std::cout << std::endl;
-    // std::cout << horner_evaluate((complex)(2.0 / 3.0), deflated);
 
     return 0;
 }
